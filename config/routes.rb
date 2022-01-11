fbf05706fb0f4f36bff0f818e8433ffec7f1e28d
marika441 <s.marika441@gmail.com>
2022-01-11 08:23:53 +0000

    [Add]いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/home/about" => "homes#about"
   resources :declutters do
     resources :declutter_comments, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
   end
 
 end
