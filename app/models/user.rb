40d86cf21d8089d2a8f6b1df89cbb28f8e170f50
marika441 <s.marika441@gmail.com>
2022-01-17 15:21:28 +0000

    [Add]フォロー・フォロワー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,25 @@ class User < ApplicationRecord
   has_many :declutter_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
+  # フォローする・されたの関係
+  # class_name: Rslationshipテーブルを参照
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # フォロー・フォロワー一覧画面
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローをしているかどうかを確認
+  def following?(user)
+    followings.include?(user)
+  end
 
   attachment :profile_image
 
