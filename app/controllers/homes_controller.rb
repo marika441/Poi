0fe184d9c31b30f3cc8766d903bed9dc3b666ad4
marika441 <s.marika441@gmail.com>
2022-01-17 12:24:18 +0000

    [Add]ゲストログイン機能追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,19 @@ class HomesController < ApplicationController
 
   def top
   end
-  
+
   def about
+  end
+
+  def new_guest
+    # emailでユーザーが見つからない場合に作成
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      # ランダムで文字列を生成
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+    sign_in user
+    redirect_to declutters_path
   end
 
 end
