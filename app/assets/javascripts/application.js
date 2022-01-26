770f783963b661a6e9502a7358c49029748dc468
marika441 <s.marika441@gmail.com>
2022-01-26 08:43:41 +0000

    [Add]画像プレビュー機能追加[Fix]各フォームのデザインを修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,3 +58,18 @@ $(document).on('turbolinks:load', function() {
     });
   });
 });
+
+// 画像のプレビューを表示
+$(function() {
+    $('#file').on('change', function() {
+        var selectedFile = this.files[0];
+        var fileReader = new FileReader();
+
+        fileReader.onload = function(e) {
+            var loadedImageUri = e.target.result;
+             $('.image-preview').attr('src', loadedImageUri);
+        };
+
+        fileReader.readAsDataURL(selectedFile);
+    });
+});
