eb2bee5c785784bab9e9b27ceea702ab423b27e9
marika441 <s.marika441@gmail.com>
2022-01-28 09:27:14 +0000

    [Add]投稿・コメント一覧に投稿時間を表示[Fix]topページの写真を差し替え

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# CSVからnext_levelテーブルのレベル・閾値を取り込む
+# CSVからnext_levelテーブルのレベル・閾値を取り込み
 require "csv"
  
 CSV.foreach('db/next_level.csv') do |info|
   NextLevel.create(:level => info[0], :thresold => info[1])
 end
+
+# User
+User.create!(:name => "はなこ", :email => "example@example.com", :password => "123456")
+User.create!(:name => "たろう", :email => "example2@example.com", :password => "123456")
+User.create!(:name => "ぽい", :email => "example3@example.com", :password => "123456")
