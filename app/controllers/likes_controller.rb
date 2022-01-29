4d8d26075ccfd2ac721b11f7d5c4bdd64a581099
marika441 <s.marika441@gmail.com>
2022-01-29 03:45:54 +0000

    [Fix]各コントローラーのリファクタリング・viewの部分テンプレート化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
+  before_action :set_like, only: [:create, :destroy]
 
   def create
-    @declutter = Declutter.find(params[:declutter_id])
     @like = @declutter.likes.new(user_id: current_user.id)
     @like.save
     # render先にjsファイルを指定
@@ -9,11 +9,16 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @declutter = Declutter.find(params[:declutter_id])
     @like = @declutter.likes.find_by(user_id: current_user.id)
     @like.destroy
     # render先にjsファイルを指定
     render :likes
+  end
+  
+  private
+  
+  def set_like
+    @declutter = Declutter.find(params[:declutter_id])
   end
   
 end
